1468f4ae7bda4afa9c58e5f49f8c8e8a4294594a
Kayemba Luwaga <76554604+KayembaLuwaga@users.noreply.github.com>
2023-11-05 04:07:06 +0300

    Attribute error correction
    Attribute error correction

--- audio_processing/sibilance_removal.py
+++ audio_processing/sibilance_removal.py
@@ -7,20 +7,20 @@ from pydub.playback import play
 def remove_sibilance(audio, silence_thresh):
     # Convert AudioSegment to NumPy array
     audio_array = np.array(audio.get_array_of_samples())
-    
+
     # Apply spectral analysis using Librosa
     stft = librosa.stft(audio_array.astype(float))
-    
+
     # Find sibilant frequency components based on threshold
     sibilance_mask = np.abs(stft) > silence_thresh
-    
+
     # Remove sibilant components
     stft_clean = stft * ~sibilance_mask
-    
+
     # Inverse transform to obtain clean audio
     audio_clean = librosa.istft(stft_clean)
-    
+
     # Convert back to AudioSegment
-    audio_clean = AudioSegment.from_array(audio_clean.astype(np.int16), frame_rate=audio.frame_rate, sample_width=2, channels=1)
-    
+    audio_clean = AudioSegment(audio_clean.tobytes(), frame_rate=audio.frame_rate, sample_width=2, channels=1)
+
     return audio_clean
